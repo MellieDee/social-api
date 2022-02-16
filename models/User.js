28a9d0b65e6dc4ee3390d94b509378ab68d1662f
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-02-15 20:40:20 -0800

    finalize email regex match

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-// let validator = require('validator');
+// const validator = require('validator');
 
 const UserSchema = new Schema(
   {
@@ -12,9 +12,9 @@ const UserSchema = new Schema(
     email: {
       type: String,
       unique: true,
-      required: 'Email is required.',
-      //validate: [validateEmail, 'Please fill a valid email address'],
-      // match: [/^[A-Z0-9.+-]+[A-Z0-9.+-]+@[A-Z0-9.-]{2,63}\.([a-z]{2,6})$/, 'Please enter a valid email address']
+      required: 'Please enter a valid email.',
+      // validate: [validateEmail, 'Please enter a valid email address'],
+      match: [/^[a-zA-Z0-9.+-]+[a-zA-Z0-9.+-]?@[a-zA-Z0-9.-]{2,63}\.([a-zA-Z]{2,6}[a-zA-Z0-9.]?)$/g]
       //match: [/.+@.+\..+/]
       // lowercase: true,
       // validate: (value) => {
